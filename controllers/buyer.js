f6104454a071f67d9a96bcbbc5db22c2cab2c3af
Edilson <eddylson.londonio@gmail.com>
2020-12-26 16:09:28 -0500

    crud

--- controllers/buyer.js
+++ controllers/buyer.js
@@ -18,6 +18,42 @@ async function createBuyer (req, res, next) {
   }
 }
 
+async function updateBuyer( req, res, next ){
+	const { uuid } = req.params
+	const { name } = req.body
+	try{
+		const updatedBuyer = await ModelBuyer.update({email},{
+			where: {
+				buyer_id: uuid
+			}
+		})
+		return res.status(200).send({
+			ok: true,
+			updatedBuyer
+		})
+
+	}catch(e){
+		next(e)
+	}
+}
+
+async function deleteBuyer(req,res,next){
+	const { uuid } = req.params
+	try{
+		const removedBuyer = await ModelBuyer.destroy({
+			buyer_id: uuid
+		})
+		return res.status(200).send({
+			ok: true,
+			removedBuyer
+		})
+	}catch(e){
+		next(e)
+	}
+}
+
 module.exports = {
-  createBuyer
+	createBuyer,
+	updateBuyer,
+	deleteBuyer
 }
--- controllers/seller.js
+++ controllers/seller.js
@@ -37,6 +37,46 @@ async function createSeller (req, res, next) {
   }
 }
 
+async function updatesSeller(req, res, next) {
+	const { uuid } = req.params
+	const { name , address} = req.body
+	try{
+		const updatedSeller = await ModelSeller.update({
+			name,
+			address
+		}, {
+			where:{
+				seller_id: uuid
+			}
+		})
+		return res.status(200).send({
+			ok:true,
+			updatedSeller
+		})
+
+	}catch(e){
+		next(e)
+	}
+}
+
+async function deleteSeller(req, res, next) {
+	const { uuid } = req.params
+	try{
+		const removedSeller = await ModelProduct.destroy({
+			where: {
+				product_id: uuid
+			}
+		})
+		return res.status(200).send({
+			ok: true,
+			removedSeller
+		})
+
+	}catch(e){
+		next(e)
+	}
+}
+
 module.exports = {
   createUser,
   createSeller
