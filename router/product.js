d6807409365e2ff8ba6624797f4c0217a3445b56
Edilson <eddylson.londonio@gmail.com>
2020-12-25 20:54:11 -0500

    Products by seller

--- router/product.js
+++ router/product.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/product')
 
 router.post('/product', controller.createProduct)
 router.get('/products', controller.getProducts)
+router.get('/products/:uuid',controller.getProductsBySeller)
 
 module.exports = router
