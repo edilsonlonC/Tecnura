5947483d7cfbeec00987ef4ce9beab8144d2e9e8
Edilson <eddylson.londonio@gmail.com>
2020-12-27 20:01:55 -0500

    new router for orders

---
+++ router/order.js
@@ -0,0 +1,10 @@
+'use strict'
+const router = require('express').Router()
+const controller = require('../controllers/order')
+router.post('/order',controller.createOrder)
+router.get('/order/:uuid', controller.getOrder)
+router.get('/order/seller/:uuid', controller.getOrdersBySeller)
+router.get('/order/buyer/:uuid', controller.getOrderByBuyer)
+
+
+module.exports = router
