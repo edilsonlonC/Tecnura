8433f866ae3865dbb206da9dfc23ebe7d6196ade
Edilson <eddylson.londonio@gmail.com>
2020-12-27 19:59:27 -0500

    new routes for crud

--- router/seller.js
+++ router/seller.js
@@ -4,4 +4,6 @@ const controller = require('../controllers/seller')
 
 router.get('/user', controller.createUser)
 router.post('/seller', controller.createSeller)
+router.put('/seller/:uuid', controller.updatesSeller)
+router.delete('/seller/:uuid', controller.deleteSeller)
 module.exports = router
