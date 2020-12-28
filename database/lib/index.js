9d37c43767fde241d7827244ad13ee4ed48bfa17
Edilson <eddylson.londonio@gmail.com>
2020-12-27 19:56:14 -0500

    associated orders

--- database/lib/index.js
+++ database/lib/index.js
@@ -1,20 +1,66 @@
 'use strict'
 const ModelSeller = require('../../models/seller')()
 const ModelProduct = require('../../models/product')()
+const ModelBuyer = require('../../models/buyer')()
+const ModelOrder = require('../../models/order')()
+
 const debug = require('debug')('tecnura:db')
 const { DataTypes } = require('sequelize')
 module.exports = function () {
-  debug('in lib')
   ModelSeller.hasMany(ModelProduct, {
-    type: DataTypes.UUID,
-    foreignKey: 'SellerId'
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'SellerId'
+		}
   })
-  ModelProduct.belongsTo(ModelSeller, {
-    type: DataTypes.UUID,
-    foreignKey: 'SellerId'
+  ModelProduct.belongsTo(ModelSeller,{
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'SellerId'
+		}
   })
+	ModelSeller.hasMany(ModelOrder, {
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'SellerId'
+		}
+  })
+	ModelOrder.belongsTo(ModelSeller,{
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'SellerId'
+		}
+  })
+	ModelBuyer.hasMany(ModelOrder,{
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'BuyerId'
+		}
+  })
+	ModelOrder.belongsTo(ModelBuyer,{
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'BuyerId'
+		}
+	})
+	ModelProduct.hasMany(ModelOrder,{
+		foreignKey:{
+			type: DataTypes.UUID,
+			name: 'ProductId'
+		}
+	})
+	ModelOrder.belongsTo(ModelProduct,{
+		foreignKey: {
+			type: DataTypes.UUID,
+			name: 'ProductId'
+		}
+	})
+
+  debug('relations created')
   return {
     ModelProduct,
-    ModelSeller
+		ModelSeller,
+		ModelBuyer,
+		ModelOrder
   }
 }
