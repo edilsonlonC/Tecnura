5ebf7922c3801dc18473b415456f459b5b1cea1c
Edilson <eddylson.londonio@gmail.com>
2021-01-02 14:03:30 -0500

    verify token

--- auth/index.js
+++ auth/index.js
@@ -9,8 +9,9 @@ function generateJwt (data,secret) {
 	return token
 }
 
-function verifyToken (token,secret){
+function verifyToken (token){
 	try{
+		const secret = configJWT.token.JWTSecret
 		const decoded = jwt.verify(token, secret)
 		return decoded
 	}catch(e){
@@ -19,18 +20,28 @@ function verifyToken (token,secret){
 	}
 }
 
+function verifyRefreshToken (token){
+	try{
+		const secret = configJWT.refrest_token.JWTSecret
+		const decoded = jwt.verify(token, secret)
+		return decoded
+	}catch(e){
+		debug(e)
+		return false
+	}
+}
 function getToken(options){
 	let tokens = {}
 	if ( options.token ){
 		const secret = configJWT.token.JWTSecret
-		const token = generateJwt(options.token.data,secret)
+		const token = generateJwt(options.token,secret)
 		debug(options.token.data,secret)
 		tokens = { ...tokens, token }
 	}
 	if ( options.refreshToken ){
 		const secret = configJWT.refrest_token.JWTSecret
-		debug(secret,options.refreshToken.data)
-		const refreshToken = generateJwt(options.refreshToken.data,secret)
+		debug(secret,options.refreshToken)
+		const refreshToken = generateJwt(options.refreshToken,secret)
 		tokens = { ...tokens, refreshToken }
 	}
 	return tokens
@@ -39,6 +50,7 @@ function getToken(options){
 module.exports = {
 	generateJwt,
 	verifyToken,
-	getToken
+	getToken,
+	verifyRefreshToken
 
 }
