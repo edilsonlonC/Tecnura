62aba5eb1bfb49916e87d686bc329c17f7acf9d9
Edilson <eddylson.londonio@gmail.com>
2021-01-01 20:05:55 -0500

    get token and refresh token for login

--- auth/index.js
+++ auth/index.js
@@ -1,24 +1,44 @@
 'use strict'
-
+const fs = require('fs')
 const jwt = require('jsonwebtoken')
 const configJWT = require('../config/conf-auth')
-const { JWTSecret, expiresIn } = configJWT
+const debug = require('debug')('tecnura:auth')
 
-function generateJwt (data) {
-	let token = jwt.sign(data,JWTSecret,{ expiresIn })
+function generateJwt (data,secret) {
+	let token = jwt.sign(data,secret,{ expiresIn: 30 })
 	return token
 }
 
-function verifyToken (token){
+function verifyToken (token,secret){
 	try{
-		const decoded = jwt.verify(token, JWTSecret)
+		const decoded = jwt.verify(token, secret)
 		return decoded
 	}catch(e){
+		debug(e)
 		return false
 	}
 }
 
+function getToken(options){
+	let tokens = {}
+	if ( options.token ){
+		const secret = configJWT.token.JWTSecret
+		const token = generateJwt(options.token.data,secret)
+		debug(options.token.data,secret)
+		tokens = { ...tokens, token }
+	}
+	if ( options.refreshToken ){
+		const secret = configJWT.refrest_token.JWTSecret
+		debug(secret,options.refreshToken.data)
+		const refreshToken = generateJwt(options.refreshToken.data,secret)
+		tokens = { ...tokens, refreshToken }
+	}
+	return tokens
+}
+
 module.exports = {
 	generateJwt,
-	verifyToken
+	verifyToken,
+	getToken
+
 }
--- config/conf-auth.js
+++ config/conf-auth.js
@@ -5,7 +5,7 @@ module.exports = {
 		algorithm: process.env.algorithm || 'RS256'
 	},
 	refrest_token:{
-		JWTSecret: process.env.JWTSecret || 'secret password jwt',
+		JWTSecret: process.env.JWTSecret || 'secret password jwt for refresh token',
 		expiresIn: process.env.expriresIn || '1h',
 		algorithm: process.env.algorithm || 'RS256'
 	},
